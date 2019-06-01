5e5a4b32f9703e5930cce08ff4b03a7d95cf2ed5
4bic <robyne.kiplangat@gmail.com>
2019-06-01 11:36:56 +0300

    Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => (
+    <header className="bg-green sans-serif center pa4 pb2 pt5-ns ph7-l">
+        <h1
+            className="f2 f1-m f-headline-l measure-narrow lh-title mv0">
+            <span
+                className="bg-black-90 lh-copy center white pa1 tracked-tight">
+                &nbsp; Forest Slider &nbsp;
+        </span>
+        </h1>
+        <h4 className="f3 fw1 georgia i">
+            Kenya Forests Deforestation Over Time
+      </h4>
+    </header>
+)
+
+export default Header;
