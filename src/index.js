1e4b915020f6cc38052baa13766ab9269b623fa9
4bic <robyne.kiplangat@gmail.com>
2019-06-01 19:37:01 +0300

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import 'tachyons/css/tachyons.css'
-
 import App from './App';
+import 'tachyons/css/tachyons.css'
 import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
