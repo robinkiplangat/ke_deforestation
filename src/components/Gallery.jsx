439aace846411ce3c0903352718ccdf3937b07db
4bic <robyne.kiplangat@gmail.com>
2019-06-01 15:50:16 +0300

    gallery template placeholder

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from './logo.svg';
+   
+const Gallery = () => ( 
+    <div className="slider">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+        );
+export default Gallery;
