ee716676f7d032669177e1d3f83327511027a495
4bic <robyne.kiplangat@gmail.com>
2019-06-01 12:44:49 +0300

    incorporate tachyons css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Header from './components/Header'
 
-function App() {
-    return (
+
+class App extends Component {
+    render(){
+        return (
         <div className="App">
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        Get Started
-        </a>
-        </header>
-        </div>
-    );
-}
+            <Header />
 
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Edit <code>src/App.js</code> and save to reload.</p>
+             <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Get Started
+             </a>
+            </header>
+        </div>
+
+
+
+        );
+    }
+}
 export default App;
