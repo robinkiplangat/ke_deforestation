395f59c968e6fcf4f4be5b1b530f0f7d519bcf4e
4bic <robyne.kiplangat@gmail.com>
2019-06-13 13:54:52 +0300

    reference img url by ID

---
+++ src/components/data/datake.js
@@ -0,0 +1,196 @@
+const forestData = JSON.parse(
+[
+     {
+        "Forest": "Aberdares",
+        "ImageID": 345596,
+        "Description": "Aberdares- Forest Description"
+      },
+      {
+        "Forest": "Arabuko",
+        "ImageID": 345616,
+        "Description": "Arabuko- Forest Description"
+      },
+      {
+        "Forest": "Boni",
+        "ImageID": 345620,
+        "Description": "Boni- Forest Description"
+      },
+      {
+        "Forest": "Gaikuyu",
+        "ImageID": 345623,
+        "Description": "Gaikuyu- Forest Description"
+      },
+      {
+        "Forest": "Kapcherop",
+        "ImageID": 345751,
+        "Description": "Kapcherop- Forest Description"
+      },
+      {
+        "Forest": "Karura",
+        "ImageID": 345756,
+        "Description": "Karura- Forest Description"
+      },
+      {
+        "Forest": "Kikuyu Escarpment",
+        "ImageID": 345766,
+        "Description": "Kikuyu Escarpment- Forest Description"
+      },
+      {
+        "Forest": "Kipkunurr",
+        "ImageID": 345773,
+        "Description": "Kipkunurr- Forest Description"
+      },
+      {
+        "Forest": "Kitalale",
+        "ImageID": 345782,
+        "Description": "Kitalale- Forest Description"
+      },
+      {
+        "Forest": "Loima",
+        "ImageID": 345790,
+        "Description": "Loima- Forest Description"
+      },
+      {
+        "Forest": "Lorogi",
+        "ImageID": 345801,
+        "Description": "Lorogi- Forest Description"
+      },
+      {
+        "Forest": "Lugari",
+        "ImageID": 345810,
+        "Description": "Lugari- Forest Description"
+      },
+      {
+        "Forest": "Lugari and Turbo",
+        "ImageID": 345834,
+        "Description": "Lugari and Turbo- Forest Description"
+      },
+      {
+        "Forest": "Maluganji",
+        "ImageID": 345836,
+        "Description": "Maluganji- Forest Description"
+      },
+      {
+        "Forest": "Marenji",
+        "ImageID": 345860,
+        "Description": "Marenji- Forest Description"
+      },
+      {
+        "Forest": "Marmanet",
+        "ImageID": 345915,
+        "Description": "Marmanet- Forest Description"
+      },
+      {
+        "Forest": "Marsabit",
+        "ImageID": 345917,
+        "Description": "Marsabit- Forest Description"
+      },
+      {
+        "Forest": "Mathew's Range",
+        "ImageID": 345924,
+        "Description": "Mathew's Range- Forest Description"
+      },
+      {
+        "Forest": "Mau",
+        "ImageID": 345959,
+        "Description": "Mau- Forest Description"
+      },
+      {
+        "Forest": "Menengai",
+        "ImageID": 345996,
+        "Description": "Menengai- Forest Description"
+      },
+      {
+        "Forest": "Mt. Elgon",
+        "ImageID": 346020,
+        "Description": "Mt. Elgon- Forest Description"
+      },
+      {
+        "Forest": "Mt. Kenya",
+        "ImageID": 345731,
+        "Description": "Mt. Kenya- Forest Description"
+      },
+      {
+        "Forest": "Mt. Ng'iro",
+        "ImageID": 346062,
+        "Description": "Mt. Ng'iro- Forest Description"
+      },
+      {
+        "Forest": "Mukogodo",
+        "ImageID": 346036,
+        "Description": "Mukogodo- Forest Description"
+      },
+      {
+        "Forest": "Mutito",
+        "ImageID": 346040,
+        "Description": "Mutito- Forest Description"
+      },
+      {
+        "Forest": "Mutula",
+        "ImageID": 346042,
+        "Description": "Mutula- Forest Description"
+      },
+      {
+        "Forest": "Namanga",
+        "ImageID": 346048,
+        "Description": "Namanga- Forest Description"
+      },
+      {
+        "Forest": "Ndoto",
+        "ImageID": 346052,
+        "Description": "Ndoto- Forest Description"
+      },
+      {
+        "Forest": "Ngaya",
+        "ImageID": 346059,
+        "Description": "Ngaya- Forest Description"
+      },
+      {
+        "Forest": "Ngong Hills",
+        "ImageID": 346066,
+        "Description": "Ngong Hills- Forest Description"
+      },
+      {
+        "Forest": "Ngong Road",
+        "ImageID": 346072,
+        "Description": "Ngong Road- Forest Description"
+      },
+      {
+        "Forest": "Ololua",
+        "ImageID": 346075,
+        "Description": "Ololua- Forest Description"
+      },
+      {
+        "Forest": "Sekhendu",
+        "ImageID": 346089,
+        "Description": "Sekhendu- Forest Description"
+      },
+      {
+        "Forest": "Shimba Hills",
+        "ImageID": 346079,
+        "Description": "Shimba Hills- Forest Description"
+      },
+      {
+        "Forest": "Tinderet",
+        "ImageID": 346098,
+        "Description": "Tinderet- Forest Description"
+      },
+      {
+        "Forest": "Lembus",
+        "ImageID": 346098,
+        "Description": "Lembus- Forest Description"
+      },
+      {
+        "Forest": "Londiani",
+        "ImageID": 346098,
+        "Description": "Londiani- Forest Description"
+      },
+      {
+        "Forest": "",
+        "ImageID": "",
+        "Description": ""
+      }
+  ]
+  );
+
+export default forestData;
