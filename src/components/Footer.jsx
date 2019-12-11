adda70d78aa2bcb29b20de73eb93f6644f85ae2b
4bic <robyne.kiplangat@gmail.com>
2019-12-11 14:05:42 +0300

    footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,4 @@
+const Footer=()=>(<footer className='bg-primary p-2'>
+    <p className='text-white text-center text-lg py-2 lh-2'>Deforestation Ke @{new Date().getFullYear()}. All rights reserved.</p>
+</footer>)
+export default Footer;
